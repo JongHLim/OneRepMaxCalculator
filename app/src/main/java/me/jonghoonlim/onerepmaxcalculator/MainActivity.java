6e44173e1e6d3ea77de2579ee5be3e9fa54add50
Jong Hoon Lim <jhlim84@hotmail.com>
2015-10-07 16:53:11 -0500

    added some functionalities to main page

--- app/src/main/java/me/jonghoonlim/onerepmaxcalculator/MainActivity.java
+++ app/src/main/java/me/jonghoonlim/onerepmaxcalculator/MainActivity.java
@@ -4,13 +4,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int max;
+    int weight;
+    int reps;
+    String userWeightString, userRepsString;
+    EditText userWeight, userReps;
+    Button calcButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calcButton = (Button)findViewById(R.id.calculate);
+        userWeight = (EditText)findViewById(R.id.userInputWeight);
+        userReps = (EditText)findViewById(R.id.userInputReps);
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+
+            // calculate the one rep max and display it
+            public void onClick(View v) {
+                // get the user input weight
+                userWeightString = userWeight.getText().toString();
+                try {
+                    weight = Integer.parseInt(userWeightString);
+                } catch (NumberFormatException e){
+                    // not an Integer
+                }
+
+                // get the user input reps
+                userRepsString = userReps.getText().toString();
+                try {
+                    reps = Integer.parseInt(userRepsString);
+                } catch (NumberFormatException e){
+                    // not an Integer
+                }
+
+                max = weight + reps;
+            }
+
+        });
     }
 
     @Override
